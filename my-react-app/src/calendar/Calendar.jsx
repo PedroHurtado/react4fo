b9c844f8f7e60fcf384b0fef63699d0989e5c78e
Pedro Hurtado <pedrohurt@gmail.com>
2025-09-17 11:06:49 +0200

    calendar

--- my-react-app/src/calendar/Calendar.jsx
+++ my-react-app/src/calendar/Calendar.jsx
@@ -1,6 +1,21 @@
 import './Calendar.css'
+
+function* getDays(){
+    for(let i=0;i<31;i++)    
+        yield i+1
+}
 function Calendar(){
-    return (<div>Calendar</div>)
+    
+    const days = [...getDays()]
+    return (
+        <div className='calendar'>
+            {
+                days.map(day=>{
+                    return <div className='day' key={day}>{day}</div>
+                })
+            }
+        </div>
+    )
 }
 
 export default Calendar
